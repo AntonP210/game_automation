d3f669297c9e6e11afbddd3bc22db351f743c3de
Anton Pasternak <74300994+AntonP210@users.noreply.github.com>
2024-07-18 11:11:53 +0300

    added openCV to stabilize the tests

--- test_automation.py
+++ test_automation.py
@@ -3,48 +3,42 @@ import pytest
 import os
 
 # Import the functions from the automation script
-from automation_helpers import wait_for_element, open_program , wait_and_click 
-from constants import PROGRAM_DIR
-script_dir = os.path.dirname(__file__)  # Directory of the current script
+from helpers import wait_for_element, open_program , wait_and_click 
+from constants import MEMU_DIR, MEMU_LOGO , STORE_ICON , SIGN_IN , EMAIL_PHONE , EMAIL_PHONE,  FORGOT_EMAIL, CREATE_ACCOUNT , NEXT_BTN
 
 
     
 def test_open_program():
-    program_command = PROGRAM_DIR
+    program_command = MEMU_DIR
 
     # Open the program
     open_program(program_command)
 
-    # Construct the path to the element image dynamically
-    element_path = os.path.join(script_dir, 'images', 'memu_logo.png')  
-
     # Wait for the element to appear
-    element_location = wait_for_element(element_path)
+    memu_logo = wait_for_element(MEMU_LOGO)
     
     # Assert that the element was found
-    assert element_location is not None, "Program was not found on the screen, automation is stopped."
+    assert memu_logo is not None, "Program was not found on the screen, automation is stopped."
     
 def test_open_play_store():
 
-    store_icon = os.path.join(script_dir, 'images', 'store.png')  
-    wait_and_click(store_icon)
+    wait_and_click(STORE_ICON)
     
-    sign_in_btn = os.path.join(script_dir, 'images', 'signin.png') 
     time.sleep(2)
-    wait_and_click(sign_in_btn)
+    wait_and_click(SIGN_IN)
     
 def test_login_screen():
-    time.sleep(2)
-    email_phone = os.path.join(script_dir, 'images', 'email_phone.png')  
-    assert email_phone is not None, "Email_phone was not found."
+    time.sleep(4)
+    email = wait_for_element(EMAIL_PHONE)
+    assert email is not None, "Email_phone was not found."
     
-    forgot_email = os.path.join(script_dir, 'images', 'forgot_email.png') 
+    forgot_email = wait_for_element(FORGOT_EMAIL)
     assert forgot_email is not None, "forgot_email was not found."
     
-    create_account = os.path.join(script_dir, 'images', 'create_account.png') 
+    create_account = wait_for_element(CREATE_ACCOUNT)
     assert create_account is not None, "create_account was not found."
     
-    next_btn = os.path.join(script_dir, 'images', 'next_btn.png') 
+    next_btn = wait_for_element(NEXT_BTN)
     assert next_btn is not None, "next_btn was not found."
     
 
